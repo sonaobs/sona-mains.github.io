8a8c85723a99e8789e9027afe991e95e446e84c4
sonaobs <65978573+sonaobs@users.noreply.github.com>
2021-08-24 17:31:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ function handleNextQuestion() {
     unCheckRadioButtons()
     //delays next question displaying for a second
     setTimeout(() => {
-        if (indexNumber <= 9) {
+        if (indexNumber <= 19) {
             NextQuestion(indexNumber)
         }
         else {
